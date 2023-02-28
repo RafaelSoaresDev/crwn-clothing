efa7b8755eea890e80e0bd38ab7691116d9ab0d8
RafaelSoaresDev <rafael.ol.soaress@gmail.com>
2023-02-28 19:13:21 -0300

    created sign up component, the form input component, button component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,16 +1,15 @@
-/* eslint-disable react/prop-types */
 import CategoryItem from '../category-item/category-item.component';
 
 import './directory.styles.scss';
 
 function Directory({ categories }) {
-  return (
-    <div className="directory-container">
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
-      ))}
-    </div>
-  );
+	return (
+		<div className="directory-container">
+			{categories.map(category => (
+				<CategoryItem key={category.id} category={category} />
+			))}
+		</div>
+	);
 }
 
 export default Directory;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,58 +1,71 @@
 import { initializeApp } from 'firebase/app';
 import {
-  getAuth,
-  signInWithRedirect,
-  signInWithPopup,
-  GoogleAuthProvider,
+	getAuth,
+	signInWithRedirect,
+	signInWithPopup,
+	GoogleAuthProvider,
+	createUserWithEmailAndPassword,
 } from 'firebase/auth';
-import {
-  getFirestore,
-  doc,
-  getDoc,
-  setDoc,
-} from 'firebase/firestore';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyC2aKo-16Jrfm9oJRgfM-5sZMHLE2Leca4',
-  authDomain: 'crwn-clothing-db-13fd4.firebaseapp.com',
-  projectId: 'crwn-clothing-db-13fd4',
-  storageBucket: 'crwn-clothing-db-13fd4.appspot.com',
-  messagingSenderId: '700992132064',
-  appId: '1:700992132064:web:de4d1b0c92d390698d9185',
+	apiKey: 'AIzaSyC2aKo-16Jrfm9oJRgfM-5sZMHLE2Leca4',
+	authDomain: 'crwn-clothing-db-13fd4.firebaseapp.com',
+	projectId: 'crwn-clothing-db-13fd4',
+	storageBucket: 'crwn-clothing-db-13fd4.appspot.com',
+	messagingSenderId: '700992132064',
+	appId: '1:700992132064:web:de4d1b0c92d390698d9185',
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
 
-provider.setCustomParameters({
-  prompt: 'select_account',
+googleProvider.setCustomParameters({
+	prompt: 'select_account',
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGooglePopup = () =>
+	signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+	signInWithRedirect(auth, googleProvider);
+
+//Setting up firebase's firestore
 
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth) => {
-  const userDocRef = doc(db, 'users', userAuth.uid);
+export const createUserDocumentFromAuth = async (
+	userAuth,
+	additionalInformation = {}
+) => {
+	if (!userAuth) return;
 
-  console.log(userDocRef);
+	const userDocRef = doc(db, 'users', userAuth.uid);
 
-  const userSnapShot = await getDoc(userDocRef);
+	const userSnapShot = await getDoc(userDocRef);
 
-  if (!userSnapShot.exists()) {
-    const { displayName, email } = userAuth;
-    const createdAt = new Date();
+	if (!userSnapShot.exists()) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
 
-    try {
-      await setDoc(userDocRef, {
-        displayName,
-        email,
-        createdAt,
-      });
-    } catch (error) {
-      console.log('error creating the user', error.message);
-    }
-  }
+		try {
+			await setDoc(userDocRef, {
+				displayName,
+				email,
+				createdAt,
+				...additionalInformation,
+			});
+		} catch (error) {
+			console.log('error creating the user', error.message);
+		}
+	}
+
+	return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+	if (!email || !password) return;
+
+	return await createUserWithEmailAndPassword(auth, email, password);
 };
